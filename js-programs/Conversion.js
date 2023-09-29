b17838fcf1f50159850ea47bb6a6916fe969e131
GeekRakesh <124446502+Rakesh8758@users.noreply.github.com>
2023-09-29 07:02:48 +0000

    Conversion

---
+++ js-programs/Conversion.js
@@ -0,0 +1,25 @@
+let score="34dd"
+console.log(typeof score)
+console.log(typeof(score))
+let variable=Number(score)
+console.log(variable)
+let val=null
+console.log(val);
+let val2=true;
+console.log(val2)
+let val3=false
+console.log(val3)
+let val4=undefined
+console.log(val4)
+// so conclusion is 
+// null = 0
+// Undefined = NAn
+//true =   1
+// false = 0
+ let id=""
+ let comparedId=Boolean(id);
+ console.log(comparedId);
+ // So boolean conclusion is 
+ // empty String = false
+ // Not empty String = true
+ 
