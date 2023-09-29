031ee6b6b9f212e6f564997ad98f5f694c35505d
GeekRakesh <124446502+Rakesh8758@users.noreply.github.com>
2023-09-29 06:25:34 +0000

    container configuration done

---
+++ js-programs/Variables.js
@@ -0,0 +1,6 @@
+const accountId=12344;
+let Name="Taniya";
+var AccountType="Savings";
+console.log(accountId);
+console.log([accountId,Name,AccountType]); // this format for array
+// In javaScript var variable type now is outdated because of functional repeatation the name 
