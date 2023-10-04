31d99a0ce24eca75451d8855e1e2d8bcdc25e2f3
GeekRakesh <124446502+Rakesh8758@users.noreply.github.com>
2023-10-04 09:00:23 +0000

    iife

---
+++ js-programs/iffe.js
@@ -0,0 +1,17 @@
+//iife is nothing but its only advanced version of function
+//it main uses is reduce the functions lengthy code
+
+// function num(){
+//     let x=10
+//     console.log(x)
+// }
+// num()
+// this is normal function call
+
+(function num2()
+{
+    let y=20;
+    console.log(y)
+})();
+
+// function call
